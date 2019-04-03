8ffd6511c6325ff1c42c814865af366036eaf83d
Sebastian Kapfer <sebastian.kapfer@appfolio.com>
2019-04-03 14:21:24 -0700

    validate correct view is rendered

--- test/controllers/imageurls_controller_test.rb
+++ test/controllers/imageurls_controller_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
 
-# FIXME check correct view is rendered in each case
-
 class ImageurlsControllerTest < ActionDispatch::IntegrationTest
   test 'should respond to /imageurls/new' do
     get '/imageurls/new'
     assert_response :success
+    assert_template 'new'
   end
 
   test 'creating a new URL works' do
@@ -13,6 +12,7 @@ class ImageurlsControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Imageurl.count') do
       post '/imageurls', params: { imageurl: { url: 'http://host.com/image.jpg' } }
       assert_redirected_to Imageurl.last
+      assert_template nil
     end
   end
 
@@ -21,17 +21,20 @@ class ImageurlsControllerTest < ActionDispatch::IntegrationTest
     assert url.save
     get imageurl_url(url)
     assert_response :success
+    assert_template 'show'
   end
 
   test 'showing invalid id fails' do
     post '/imageurls', params: { imageurl: { url: 'http://host.com/image.jpg' } }
     get format('/imageurls/%<id>i', id: Imageurl.last.id + 1)
     assert_response :not_found
+    assert_template nil
   end
 
   test 'creating an invalid URL fails' do
     post '/imageurls', params: { imageurl: { url: 'foobar' } }
     # FIXME check correct flash message is shown
     assert_response :unprocessable_entity
+    assert_template 'new'
   end
 end
