ef0b6515c06b63f330191f3f643f3ea54eb6d249
Sebastian Kapfer <sebastian.kapfer@appfolio.com>
2019-04-04 00:54:43 -0700

    trim down routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
 
-  # FIXME trim down routes
-  resources :imageurls
+  resources :imageurls, only: %i[new create show]
 end
