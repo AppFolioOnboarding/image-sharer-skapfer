42e5d5e1bdf66e545caeea45a7bc8910d1a72f27
Sebastian Kapfer <sebastian.kapfer@appfolio.com>
2019-04-04 14:09:45 -0700

    more idiomatic

--- test/controllers/imageurls_controller_test.rb
+++ test/controllers/imageurls_controller_test.rb
@@ -12,13 +12,12 @@ class ImageurlsControllerTest < ActionDispatch::IntegrationTest
       post '/imageurls', params: { imageurl: { url: 'http://host.com/image.jpg' } }
     end
     assert_redirected_to Imageurl.last
-    assert_equal flash[:success], 'Image URL added successfully!'
+    assert_equal 'Image URL added successfully!', flash[:success]
     assert_template nil
   end
 
   test 'showing valid id succeeds' do
-    url = Imageurl.new(url: 'http://host.com/image.jpg')
-    assert url.save
+    url = Imageurl.create!(url: 'http://host.com/image.jpg')
     get imageurl_url(url)
     assert_response :success
     assert_template 'show'
@@ -34,7 +33,7 @@ class ImageurlsControllerTest < ActionDispatch::IntegrationTest
   test 'creating an invalid URL fails' do
     post '/imageurls', params: { imageurl: { url: 'foobar' } }
     assert_response :unprocessable_entity
-    assert_equal flash[:warning], 'Url must begin with http:// or https://'
+    assert_equal 'Url must begin with http:// or https://', flash[:warning]
     assert_template 'new'
   end
 end
