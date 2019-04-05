6b55a738be467d916852056464f33261594fc705
Sebastian Kapfer <sebastian.kapfer@appfolio.com>
2019-04-05 09:57:09 -0700

    remove home action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def home
-    redirect_to imageurls_url, status: :temporary_redirect
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#home'
-
+  root to: redirect('/imageurls', status: 307)
   resources :imageurls, only: %i[new create show index]
 end
