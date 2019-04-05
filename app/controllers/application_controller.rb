9a43dce406db87312b26304849b8e0f71ec08cdb
Sebastian Kapfer <sebastian.kapfer@appfolio.com>
2019-04-05 01:32:31 -0700

    sk - replace homepage by imageurls index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def home
+    redirect_to imageurls_url, status: :temporary_redirect
+  end
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   test 'should respond to /' do
     get '/'
-    assert_response :success
+    assert_response :temporary_redirect
+    assert_redirected_to '/imageurls'
   end
 end
