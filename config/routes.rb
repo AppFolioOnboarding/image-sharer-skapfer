7f21f50ab4f2f43f82aadfc5f97b3de9f8aefa1d
Sebastian Kapfer <sebastian.kapfer@appfolio.com>
2019-04-06 04:30:05 -0700

    sk - js requested 307 -> 200 for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: redirect('/imageurls', status: 307)
   resources :imageurls, only: %i[new create show index]
+  root 'imageurls#index'
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   test 'should respond to /' do
     get '/'
-    assert_response :temporary_redirect
-    assert_redirected_to '/imageurls'
+    assert_response :success
+    assert_template 'index'
   end
 end
