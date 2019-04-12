b9fc5157b0b9840b1b7900615cb4f2204c52f856
Sebastian Kapfer <sebastian.kapfer@appfolio.com>
2019-04-12 02:13:43 -0700

    sk - use simple form for the share form

--- app/controllers/imageurl_shares_controller.rb
+++ app/controllers/imageurl_shares_controller.rb
@@ -1,34 +1,21 @@
 class ImageurlSharesController < ApplicationController
   def new
-    setup_form
+    params = self.params.permit(:imageurl_id, :to, :from, :message)
+    @imageurl_share = ImageurlShare.new(params)
+    @imageurl_share.message ||= 'Enter a friendly message here...'
   rescue ActiveRecord::RecordNotFound
     head :not_found
   end
 
   def create
-    share = ImageurlShare.new(imageurl_share_params)
-    if share.valid?
-      ImageurlMailer.with(share: share).shareimage_email.deliver_now
+    params = self.params.permit(imageurl_share: %i[imageurl_id to from message])
+    @imageurl_share = ImageurlShare.new(params[:imageurl_share])
+    if @imageurl_share.valid?
+      ImageurlMailer.with(share: @imageurl_share).shareimage_email.deliver_now
       flash[:success] = 'email sent successfully'
       redirect_to imageurls_path
     else
-      flash[:danger] = share.errors.full_messages.join('; ')
-      setup_form
       render 'new', status: :unprocessable_entity
     end
-  end
-
-  private
-
-  def imageurl_share_params
-    params.permit(:imageurl_id, :to, :from, :message)
-  end
-
-  def setup_form
-    params = imageurl_share_params
-    @imageurl = Imageurl.find(params[:imageurl_id])
-    @message = params[:message] || 'Enter a friendly message here...'
-    @to = params[:to]
-    @from = params[:from]
   end
 end
--- app/models/imageurl_share.rb
+++ app/models/imageurl_share.rb
@@ -1,8 +1,14 @@
 class ImageurlShare
   include ActiveModel::Model
   attr_accessor :to, :from, :message, :imageurl_id
-  validates :to, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'recipient email address is invalid' }
-  validates :from, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'sender email address is invalid' }
+  validates :to, presence: true, format: {
+    with: URI::MailTo::EMAIL_REGEXP,
+    message: 'recipient email address is invalid'
+  }
+  validates :from, presence: true, format: {
+    with: URI::MailTo::EMAIL_REGEXP,
+    message: 'sender email address is invalid'
+  }
   validates :imageurl_id, presence: true
   validate :imageurl_id_valid
 
